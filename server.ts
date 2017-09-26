f131cf2399c1f9078b2f5729f7de30f56381117d
potaru <jspaulbhang@gmail.com>
2017-09-26 18:47:34 +0900

    atom install

--- server.ts
+++ server.ts
@@ -12,11 +12,11 @@ declare var require;
 
 const app = express();
 app.set('views', __dirname + '/views');
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-const server = app.listen(config.port, function(){
-	console.log("Express server has started on port 3000")
+const server = app.listen(config.port, function() {
+  console.log("Express server has started on port 3000")
 });
 
 app.use(express.static('public'));
@@ -24,15 +24,15 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.use(function (err, req, res, next) {
-	console.log(err, "err");
-	res.status(500).send(err);
+app.use(function(err, req, res, next) {
+  console.log(err, "err");
+  res.status(500).send(err);
 })
 
 app.use(session({
-	secret: '@#@$MYSIGN#@$#$',
-	resave: false,
-	saveUninitialized: true
+  secret: '@#@$MYSIGN#@$#$',
+  resave: false,
+  saveUninitialized: true
 }));
 
 const router = indexRouter(app);
